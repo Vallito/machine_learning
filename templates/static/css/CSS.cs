b7171064f1131eea1114935185a5f1ec9c107d56
jwoodward904 <jwoodward904@gmail.com>
2019-03-02 13:37:21 -0500

    starting out html/CSS

---
+++ templates/static/css/CSS.cs
@@ -0,0 +1,10 @@
+body {
+  padding-top: 1.5;
+  padding-bottom: 1.5;
+  background: url("MAP background.jpg") no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+    font-family: South Park;
+}
